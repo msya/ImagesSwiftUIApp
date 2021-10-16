84f95c0319d740bfdd76718d1ccdb315f68f896e
Mohit <msya@users.noreply.github.com>
2021-10-15 23:07:00 -0400

    Initial work (#1)
    * Initial work
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update

---
+++ ImagesSwiftUIApp/ImagesSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesSwiftUIAppApp.swift
+//  ImagesSwiftUIApp
+//
+//  Created by Mohit S on 10/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ImagesSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
